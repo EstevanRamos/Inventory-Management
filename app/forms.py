f9c8568a93437b2212f5a31ce422721e57b78e33
EstevanRamos <Ramos_Estevan@outlook.com>
2022-03-22 14:14:38 -0600

    Added Bootstrap and modied db

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,4 @@
+from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField,IntegerField,SelectField,SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired,NumberRange, Email
@@ -8,3 +9,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators = [DataRequired()])
     remember = BooleanField ('Remember Me')
     submit = SubmitField ('Sign In')
+
+
+
+
+
+
+
+
+class commodityForm(FlaskForm):
+    item = StringField('itemType', validators=[DataRequired()])
+    entry_type = db.Column(db.String(10), index = True)
+    quantity = db.Column(db.String(10), index = True)
+    datein = db.Column(db.DateTime, index = True, default = datetime.utcnow)
+    dateout = db.Column(db.DateTime, index = True)
+    shipper = db.Column(db.String(60), index = True)
+    consignee = db.Column(db.String(60), index = True)
+    customer = db.Column(db.String(60), index = True)
+    notes = db.Column(db.Text)
+    user_id = db.Column(db.Integer , db.ForeignKey('user.id'))
--- app/models.py
+++ app/models.py
@@ -26,7 +26,7 @@ class Commodity(db.Model):
     consignee = db.Column(db.String(60), index = True)
     customer = db.Column(db.String(60), index = True)
     notes = db.Column(db.Text)
-    user_id = db.Column(db.Integer , db.ForeignKey('user.id'))
+    status = db.Column(db.String(60), index = True)
 
     def __repr__(self):
         return '<Commodity {}>'.format(self.item)
@@ -42,7 +42,15 @@ class Commodity(db.Model):
             'consignee': self.consignee,
             'customer': self.customer,
             'notes': self.notes,
-            'factored_by' : self.user_id
+            'status':self.status
         }
 
 
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    whatsapp = db.Column(db.Integer, index=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
